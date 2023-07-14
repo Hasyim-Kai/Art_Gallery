3fca0c6643786afa9ea742ede89dd998270511df
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-07-14 14:54:22 +0700

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,63 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+  return <main className="">
+    {/* HERO */}
+    <section className='flex flex-col lg:flex-row items-center gap-5'>
+      <div className='relative w-full lg:w-1/2 aspect-square'>
+        <Image className='art' src="/art (5).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' priority />
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='p-5'>
+        <h1 className='text-display text-7xl lg:text-8xl font-medium'>Explore <br />The<br />Dreaming <br />Masterpiece</h1>
+        <Link href="/explore"><p className='text-display text-2xl mt-5'><u>Explore</u></p></Link>
       </div>
+    </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    {/* INTRO */}
+    <section className='relative my-44'>
+      <div className='relative w-full lg:w-1/2 aspect-square mx-auto'>
+        <Image className='art' src="/art (1).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' loading='lazy' />
       </div>
-    </main>
-  )
+      <div className='absolute top-2/3 left-5 lg:left-1/3'>
+        <h1 className='text-display text-7xl lg:text-8xl'>One-Stop <br />Imagination<br />For <br /><u>Dream Cathcer</u></h1>
+      </div>
+    </section>
+
+    {/* FEATURES */}
+    <section className='flex flex-col items-center gap-44 mt-96 relative'>
+      <div className='relative w-full lg:w-1/2 aspect-square'>
+        <Image className='art' src="/art (2).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' loading='lazy' />
+      </div>
+      <div className='relative w-full lg:w-1/2 aspect-square'>
+        <Image className='art' src="/art (3).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' loading='lazy' />
+      </div>
+      <div className='relative w-full lg:w-1/2 aspect-square'>
+        <Image className='art' src="/art (4).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' loading='lazy' />
+      </div>
+      <div className='absolute lg:left-12'>
+        <h1 className='text-7xl lg:text-8xl text-white lg:text-black'>Masterpieces <br />of<br />The <br />Moment</h1>
+      </div>
+    </section>
+
+    {/* API THANKYOU */}
+    <section className='my-44'>
+      <div className='border-b-2 border-black p-5 mx-auto w-2/3 lg:w-1/3 text-center'>
+        <h1 className='text-lg lg:text-xl'>I would like to Thankyou aaaa who provide this great API to use</h1>
+        <h1 className=''>Check it out here</h1>
+        <a href="http://google.com" target="_blank" rel="noopener noreferrer">aaaaaaaaaaaa</a>
+      </div>
+    </section>
+
+    {/* ABOUT */}
+    <section className='flex flex-col lg:flex-row items-center gap-5 bg-black text-white'>
+      <div className='relative w-full lg:w-1/2 aspect-square grayscale-0'>
+        <Image className='art' src="/art (2).jpg" alt="Picture of Art" fill={true} objectFit='fill' placeholder="blur" blurDataURL='/blur-placeholder.png' loading='lazy' />
+      </div>
+      <div className=''>
+        <h1 className=''>Built by me</h1>
+        <p className=''>reach me out</p>
+      </div>
+    </section>
+  </main>
 }
